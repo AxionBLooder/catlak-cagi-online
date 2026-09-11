1f64995fe1d949b9227546444e1de9041eee2d58
AxionBLooder <canr3556@gmail.com>
2026-09-11 17:20:27 +0300

    Add quality of life runtime

---
+++ quality-of-life-patch.js
@@ -0,0 +1,4 @@
+const QOL_S=window.__catlakSupabase;
+const QOL_APP=document.querySelector('#app');
+if(!QOL_S||!QOL_APP)throw new Error('Çatlak Çağı yaşam kalitesi katmanı başlatılamadı.');
+window.__catlakQualityOfLifeReady=true;
